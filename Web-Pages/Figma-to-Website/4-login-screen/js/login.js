c07c2d9152f0fdc8fbcad40a5982e49678174346
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-20 19:08:48 -0300

    update overlay

--- Web-Pages/Figma-to-Website/4-login-screen/js/login.js
+++ Web-Pages/Figma-to-Website/4-login-screen/js/login.js
@@ -2,8 +2,7 @@
 
 const loginContainer = document.getElementById('login-container');
 
-const moveOverlay = () => loginContainer.classList.add('move');
-
-
+const moveOverlay = () => loginContainer.classList.toggle('move');
 
 document.getElementById('btn-register').addEventListener('click', moveOverlay);
+document.getElementById('btn-login').addEventListener('click', moveOverlay);
