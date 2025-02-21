bf8d8c8fbd4a10f71e0e8c1c3b03d65b3ca855f7
Carlos H. Monnerat <99290040+CarlosMonnerat@users.noreply.github.com>
2025-02-21 14:06:12 -0300

    update responsive

--- Web-Pages/Figma-to-Website/4-login-screen/js/login.js
+++ Web-Pages/Figma-to-Website/4-login-screen/js/login.js
@@ -6,3 +6,6 @@ const moveOverlay = () => loginContainer.classList.toggle('move');
 
 document.getElementById('btn-register').addEventListener('click', moveOverlay);
 document.getElementById('btn-login').addEventListener('click', moveOverlay);
+
+document.getElementById('register-mobile').addEventListener('click', moveOverlay);
+document.getElementById('login-mobile').addEventListener('click', moveOverlay);
